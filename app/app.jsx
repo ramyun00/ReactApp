0c04753a2c3008ec8d0d90eb212fb41397c0002b
ramyun00 <annaboot@gmail.com>
2017-03-02 14:08:57 -0500

    Add error modal

--- app/app.jsx
+++ app/app.jsx
@@ -12,12 +12,12 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      <Route path="about" component={About}/>
-      <Route path="examples" component={Examples}/>
-      <IndexRoute component={Weather}/>
-    </Route>
-  </Router>,
+  	<Router history={hashHistory}>
+    	<Route path="/" component={Main}>
+			<Route path="about" component={About}/>
+			<Route path="examples" component={Examples}/>
+			<IndexRoute component={Weather}/>
+    	</Route>
+  	</Router>,
   document.getElementById('app')
 );
